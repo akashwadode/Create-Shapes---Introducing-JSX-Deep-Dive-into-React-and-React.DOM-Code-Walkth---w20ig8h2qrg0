2caf3d2e5ca17b9c38f01cd85a02a90a3048f1d8
akash wadode <ajwadode25@gmail.com>
2023-10-14 20:34:08 +0530

    project completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,38 @@
-import React from 'react'
-import '../styles/App.css';
+"use client";
+import React from "react";
+import "../styles/App.css";
 const App = () => {
-
+  const [shape, setShape] = React.useState("square");
+  const [list, setList] = React.useState([]);
+  const handleSelect = (e) => {
+    setShape(e.target.value);
+  };
+  const addShape = ()=>{
+    // console.log(shape);
+    setList([...list,shape]);
+    console.log(typeof(list));
+  }
+  const RenderElement = () => {
+    return (
+      list.map((item,index)=>{
+        return (<div className={item}>{index}</div>);
+      })
+    )
+  };
   return (
     <div id="main">
       <div id="shape-creator">
-        
-
+        <select onChange={handleSelect}>
+          <option value="square">Square</option>
+          <option value="circle">Circle</option>
+        </select>
+        <button onClick={addShape}>Add Shape</button>
       </div>
       <div id="shapes-holder">
+        <RenderElement />
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
